344a8d75c6f122f1cf9d5b3978d0e0b85bd33e62
Ioan23 <75176548+Ioan23@users.noreply.github.com>
2022-12-17 10:39:44 +0200

    FirstExercisePR

--- src/FirstExercise.java
+++ src/FirstExercise.java
@@ -2,5 +2,29 @@
 // Exercitiul 1
 // Afiseaza primul numar par dintr-o lista
 
+import java.util.Arrays;
+import java.util.List;
+
 public class FirstExercise {
+
+    // Instantiating List using Arrays.asList()
+    List<Integer> list = Arrays.asList(1,4,6,3,2,1,6,7,8,22,44,32,43,55);
+
+//    int evenNumber = 2;
+    int firstEvenNumberList = list.get(0);
+
+    if(firstEvenNumberList%2==0){
+        firstEvenNumberList=
+
+
+    }
+
+    public int showFirstEvenNumber(int evenNumber){
+        this.evenNumber=evenNumber;
+        return evenNumber;
+    }
+
+
+
+
 }
