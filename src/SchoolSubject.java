caba6f73d549280b3eccec9003709e2bb013256b
Ioan23 <75176548+Ioan23@users.noreply.github.com>
2022-12-19 15:45:17 +0200

    #6 Exercise solved

---
+++ src/SchoolSubject.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+//public class SchoolSubject {
+//    public String name;
+//    public List<Integer> notes;
+//
+//    SchoolSubject(String name, List<Integer> notes){
+//        this.name=name;
+//        this.notes = notes;
+//
+//    }
+//
+//}
--- src/SixthExercise.java
+++ src/SixthExercise.java
@@ -2,5 +2,42 @@
 // Exercitiul 6
 // Media generala a unui elev la 10 materii
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SixthExercise {
+
+//    int averageNotes = 0;
+
+
+//    List<Integer> numbers = new ArrayList<>();
+
+//    List<Integer> ionNotesList = Arrays.asList(7,8,6,9,8,6,6,10,9,6);
+//    List<Integer> dorelNotesList = Arrays.asList(6,8,6,9,9,7,6,9,5,6);
+//    List<Integer> GeorgeNoteList = Arrays.asList(7,8,6,10,6,10,6,10,9,6);
+//    List<Integer> LaurentiuNotesList = Arrays.asList(7,8,6,9,8,6,6,7,9,6);
+//    List<Integer> DumitruNotesList = Arrays.asList(9,9,10,9,10,8,10,10,9,9);
+
+
+//    Student myStudent = new Student("Ion",1, ionNotesList);
+//    Student myStudent2 = new Student("Dorel",1, dorelNotesList);
+//    Student myStudent3 = new Student("George",1, GeorgeNoteList);
+//    Student myStudent4 = new Student("Laurentiu",1, LaurentiuNotesList);
+//    Student myStudent5 = new Student("Dumitru",1, DumitruNotesList);
+
+
+//    for (int number:ionNotesList) {
+//        averageNotes += number;
+//    }
+
+
 }
+
+
+
+
+
+
+
+
