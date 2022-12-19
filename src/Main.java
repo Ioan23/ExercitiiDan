a4abe37270a5d50caa2bcda22b53e8a8f8649a1a
Ioan23 <75176548+Ioan23@users.noreply.github.com>
2022-12-19 12:19:42 +0200

    Solve #3 Exercise

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+    public static void main(String[] args) throws IllegalAccessException {
+        System.out.println("Salut Dan!");
 
 
         // First exercise
-        List<Integer> numbers = Arrays.asList(1, 3, 5, 3,8483, 111, 321, 322, 543, 123);
+        List<Integer> numbers = Arrays.asList( 3, 5, 2, 8483, 111, 321, 322, 543, 123);
 //        int firstEvenNumber = FirstExercise.findFirstEvenNumber(numbers);
 //        if (firstEvenNumber == -1) {
 //            System.out.println("No even numbers found in the list.");
@@ -19,11 +18,22 @@ public class Main {
 
         // Second Exercise
 
-        int firstDivisible7nr = SecondExercise.firstDivisibleNr7(numbers);
-        if (firstDivisible7nr == -1) {
-            System.out.println("No divizible numbers found in the list.");
+//        int firstDivisible7nr = SecondExercise.firstDivisibleNr7(numbers);
+//        if (firstDivisible7nr == -1) {
+//            System.out.println("No divizible numbers found in the list.");
+//        } else {
+//            System.out.println("The first divizible number with 7 from the list is: " + firstDivisible7nr);
+//        }
+
+
+    // ThirdExercise
+
+        int lowerNr = ThirdExercise.findLowestNumber(numbers);
+        if (lowerNr == -1) {
+            System.out.println("No numbers found in the list.");
         } else {
-            System.out.println("The first divizible number with 7 from the list is: " + firstDivisible7nr);
+            System.out.println("The lowest nr from list is: " + lowerNr);
         }
-    }
+}
+
 }
