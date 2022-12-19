5faab0d3c9a4200da5e0ea4929bfe0ea64fea93a
Ioan23 <75176548+Ioan23@users.noreply.github.com>
2022-12-19 11:39:12 +0200

    the solution to the first exercise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        // First exercise
+        List<Integer> numbers = Arrays.asList(1, 3, 5, 7, 8483,111,321,322,543,123);
+        int firstEvenNumber = FirstExercise.findFirstEvenNumber(numbers);
+        if (firstEvenNumber == -1) {
+            System.out.println("No even numbers found in the list.");
+        } else {
+            System.out.println("The first even number in the list is: " + firstEvenNumber);
+        }
     }
 }
+
+
+
+
+
+
+
