8b8e47e9f407bf909173b9d4afbdb4edecc4b0aa
Ioan23 <75176548+Ioan23@users.noreply.github.com>
2022-12-19 17:00:02 +0200

    #11 solve exercises

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Exercitiul 11
+        // Cel mai mare divizor comun
+
+        int a = 50;
+        int b = 100;
+
+        int gcf = gcd(a, b);
+        System.out.println(gcf);  
+    }
+
+    // calculate the greatest common divisor of numbers
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
     }
 }
