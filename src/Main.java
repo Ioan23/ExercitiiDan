946627363fe89d8ba927fbe7e4b127a2f556e6dd
Ioan23 <75176548+Ioan23@users.noreply.github.com>
2022-12-19 11:50:36 +0200

    the solution to the second exercise show first number wich is divisible with 7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         System.out.println("Hello world!");
+=======
+
+
+        // First exercise
+        List<Integer> numbers = Arrays.asList(1, 3, 5, 3,8483, 111, 321, 322, 543, 123);
+//        int firstEvenNumber = FirstExercise.findFirstEvenNumber(numbers);
+//        if (firstEvenNumber == -1) {
+//            System.out.println("No even numbers found in the list.");
+//        } else {
+//            System.out.println("The first even number in the list is: " + firstEvenNumber);
+//        }
+
+
+        // Second Exercise
+
+        int firstDivisible7nr = SecondExercise.firstDivisibleNr7(numbers);
+        if (firstDivisible7nr == -1) {
+            System.out.println("No divizible numbers found in the list.");
+        } else {
+            System.out.println("The first divizible number with 7 from the list is: " + firstDivisible7nr);
+        }
+>>>>>>> Stashed changes
     }
 }
