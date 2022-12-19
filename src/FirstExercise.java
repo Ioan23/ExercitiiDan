166fe2819cc25127c75289f3210d65783a24d44a
Ioan23 <75176548+Ioan23@users.noreply.github.com>
2022-12-19 12:34:52 +0200

    #4 Exercise solved

--- src/FirstExercise.java
+++ src/FirstExercise.java
@@ -7,22 +7,7 @@ import java.util.List;
 
 public class FirstExercise {
 
-    // Instantiating List using Arrays.asList()
-    List<Integer> list = Arrays.asList(1,4,6,3,2,1,6,7,8,22,44,32,43,55);
 
-//    int evenNumber = 2;
-    int firstEvenNumberList = list.get(0);
-
-    if(firstEvenNumberList%2==0){
-        firstEvenNumberList=
-
-
-    }
-
-    public int showFirstEvenNumber(int evenNumber){
-        this.evenNumber=evenNumber;
-        return evenNumber;
-    }
 
 
 
--- src/FourthExercise.java
+++ src/FourthExercise.java
@@ -2,5 +2,24 @@
 // Exercitiul 4
 // Afiseaza numarul cel mai mare dintr-o lista
 
+import java.util.List;
+
 public class FourthExercise {
+
+    public static int higherNumber(List<Integer> numbers){
+        // setez primul numar din lista ca fiind cel mai mare
+         int higherNr=  numbers.get(0);
+
+         for(int i = 1; i < numbers.size();i++){
+              int current = numbers.get(i);
+
+
+              //daca numarul din lista e mai mare ca numarul mai mare setat il inlocuiesc cu numarul pe care l-am avut mai mare
+              if (current> higherNr){
+
+                  higherNr = current;
+              }
+         }
+    return higherNr;
+    }
 }
