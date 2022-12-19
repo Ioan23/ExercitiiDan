4459e35e6c48cd0ffde8978f1915e52395f3bf5d
Ioan23 <75176548+Ioan23@users.noreply.github.com>
2022-12-19 16:36:01 +0200

    #9 Solve exercise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Exercitiul 9
+        // Afiseaza toate numerele pare mai mici de 100
+
+        int i = 1;
+        while (i <= 100) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+            i++;
+        }
     }
 }
