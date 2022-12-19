825a85de07ce8164cf1534318f225af52a9b4998
Ioan23 <75176548+Ioan23@users.noreply.github.com>
2022-12-19 16:33:32 +0200

    #8 Solve exercise

--- src/EighthExercise.java
+++ src/EighthExercise.java
@@ -2,5 +2,18 @@
 // Exercitiul 8
 // Afiseaza numerele pare mai mari de 100
 
-public class EighthExercise {
-}
+//import java.util.List;
+//
+//public class EighthExercise {
+//
+//    public static int findEvenNumbersHigherThan100(List<Integer> numbers) {
+//
+//
+//      for (int i = 100; i <= 200; i++) {
+//        if (i % 2 == 0) {
+//            System.out.println(i);
+//        }
+//    }
+//        return 0;
+//    }
+//}
