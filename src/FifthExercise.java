10edeb6458acd14231ca019fbaf7ff232b9f1146
Ioan23 <75176548+Ioan23@users.noreply.github.com>
2022-12-19 14:43:18 +0200

    #5 Exercise solved

--- src/FifthExercise.java
+++ src/FifthExercise.java
@@ -2,5 +2,29 @@
 // Exercitiul 5
 // Afiseaza toate numerele prime dintr-o lista
 
+import java.util.List;
+
 public class FifthExercise {
+
+
+    public static int displayPrimeNumbers(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (isPrime(number)) {
+                System.out.println(number);
+            }
+        }
+        return 0;
+    }
+
+    private static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Salut Dan");
+        List<Integer> numbers = Arrays.asList( 3, 5, 2, 8483, 111,
+                321, 322, 543, 123,47,
+                53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109,
+                113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191,
+                193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271);
+
+
+                int primeNr = FifthExercise.displayPrimeNumbers(numbers);
+       for(int i = 0; i == primeNr; i++)
+            System.out.println("");
+        }
+
     }
-}
+
+
